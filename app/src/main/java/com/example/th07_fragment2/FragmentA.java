b0d2d6c2aeea7e08f7353fb0398432c936bce2f1
HoangChi <2964139hoangchi@gmail.com>
2022-03-29 09:39:26 +0700

    finish ui

---
+++ app/src/main/java/com/example/th07_fragment2/FragmentA.java
@@ -0,0 +1,18 @@
+package com.example.th07_fragment2;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+
+public class FragmentA extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_a,container,false);
+        return view;
+    }
+}
